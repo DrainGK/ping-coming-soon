fbd4a6c6b832a1d3f1f2b44abf3d1d14b6568bcf
ロペズ ギヨム <glopez@bemap.co.jp>
2023-04-03 15:08:55 +0900

    Javascript done, project finished

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 const mail = document.getElementById("mail");
 const display = document.querySelector("#result");
+const submit = document.querySelector("#submit");
 
-function verifyEmail() {
-   
-    console.log(mail.value);
-    if (mail.value.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi)) {
-      display.innerHTML ='';
-    } else {
-        mail.classList.toggle("notvalid");
-    }
+let email;
+
+const errorDisplay = (message, valid) => {
+  if(!valid){
+    mail.style.border = "1px solid red";
+    mail.style.color = "red";
+    display.style.visibilty = "visible"
+    display.style.color = "red"
+    display.textContent = message;
+
+    console.log(display.textContent);
+  } else {
+    mail.style.border = "1px solid hsl(223, 100%, 88%)";
+    mail.style.color = "hsl(223, 100%, 88%)";
+    display.style.visibilty = "hidden"
+    display.textContent = "";
   }
+};
+
+const emailChecker = (value) => {
+  if (!value.match(/^[\w-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+    errorDisplay("Please provide a valid email address", false);
+    email = null;
+  } else {
+    errorDisplay("", true);
+    email = value;
+  }
+};
+
+mail.addEventListener("input", (e) => {
+  emailChecker(e.target.value); 
+});
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (email) {
+    // Send email to server or perform other actions
+    console.log("Email submitted: ", email);
+    alert("Thank you for subscribing!");
+  } else {
+    alert("Please provide a valid email address");
+  }
+});
